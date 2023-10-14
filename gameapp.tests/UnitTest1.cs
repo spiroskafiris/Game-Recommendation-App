4e4b3539b7edcdc694fa1350fe31361ee7d2db13
spiroskafiris <spiroskafiris@gmail.com>
2023-10-14 23:39:29 +0300

    Add project files.

---
+++ gameapp.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace gameapp.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
