29fcc60b7daa67058bef77ae2d0e918c34b4e46c
spiroskafiris <spiroskafiris@gmail.com>
2023-10-31 15:55:56 +0200

    Cleaned it up a bit

--- ReactFrontEnd/React/react-front-end/src/firebase.js
+++ ReactFrontEnd/React/react-front-end/src/firebase.js
@@ -9,13 +9,6 @@ const firebaseConfig = {
     messagingSenderId: "839519231846",
     appId: "1:839519231846:web:35908c9a9b186494a872a7",
     // measurementId: "G-22HX5Z2BQK"
-
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
